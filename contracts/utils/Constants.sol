58e4358be5141344dfa6b6d7674ea82a84812d78
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2025-07-02 22:42:16 +0200

    Added constants file

---
+++ contracts/utils/Constants.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+uint256 constant VALIDITY_DURATION = 20 seconds;
+uint256 constant TIMELOCK = 1 days;
+uint256 constant SECONDS_IN_YEAR = 365 days;
